0235c3d357c457dd220c0d9bec621d6706670d6a
sathish01121999 <sathishsrinivasan780@gmail.com>
2025-12-18 23:04:59 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,18 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Welcome {
-    public static void main(String[] args) {
-        System.out.println("Hi Hello welcome");
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hi Hello welcome";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+        server.start();
+        System.out.println("Server started on port 8080");
     }
 }
